3c9499712d55594d6d5e9868b67d8ea824d4eea5
Radosław <613162@stud.umk.pl>
2023-02-07 18:23:28 +0100

    Dodaj pliki projektów.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace EgzaminMAUI_2;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
