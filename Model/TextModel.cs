33a6a19c05657d42bed64c078b6ee8e998f2eab1
Radosław <613162@stud.umk.pl>
2023-02-07 19:13:04 +0100

    W trakcie rozwiązywania

--- Model/TextModel.cs
+++ Model/TextModel.cs
@@ -6,12 +6,16 @@ using System.Threading.Tasks;
 
 namespace EgzaminMAUI_2.Model
 {
-    public class Model
+    public class TextModel
     {
-            public string EdText { get; set; }
-            public int Length { get; set; }
-            public int Vowels { get; set; }
-            public Color LblColor { get; set; }
+        string edText;
+        int lenght, vowels;
+        Color lblColor;
+
+        public string EdText { get; set; }
+        public int Length { get { return }; set; }
+        public int Vowels { get; set; }
+        public Color LblColor { get; set; }
 
     }
 }
